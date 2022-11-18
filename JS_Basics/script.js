fcc3afb23bd7dd3cfea944029c77dab25cd4d41e
Lukas Rauch <lukas.rauch@student.htldornbirn.at>
2022-11-18 08:27:21 +0100

    variablen

---
+++ JS_Basics/script.js
@@ -0,0 +1,48 @@
+console.log("Hallo");
+
+//Variablen
+let a=7;
+let b=5;
+
+console.log("C hat den Wert " + (a+b));
+
+//Rechnungen
+
+
+// Variablen
+ a = 7;
+ b = 8;
+ c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+let result = a + b + c;
+console.log("a+b+c = "+result);
+
+// (a + b) / c
+result = (a + b) / c;
+console.log("(a + b) / c = "+result);
+
+// c -a + b
+result = c -a + b;
+console.log("c -a + b = "+result);
+
+// a + b + c * 3
+result = a + b + c * 3;
+console.log("a + b + c * 3 = "+result);
+
+// (a + b + c)/4
+result = (a + b + c)/4;
+console.log("(a + b + c)/4 = "+result);
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+// b = a + 3
+// c = c -3
+// gib die neuen Werte der Variablen aus
+a = 3;
+b = a + 3;
+c = c -3;
+
+console.log("a = "+a);
+console.log("b = "+b);
+console.log("c = "+c);
