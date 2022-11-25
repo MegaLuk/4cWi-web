72cb1259cfadb3b18a1964c4be577dedd19a4229
Lukas Rauch <lukas.rauch@student.htldornbirn.at>
2022-11-25 08:51:06 +0100

    04 Schleifen

---
+++ JS_Basics/04 Schleifen/script.js
@@ -0,0 +1,65 @@
+console.log("test");
+
+// Zähle von 7 bis 45
+
+for (let zähler = 7; zähler<46; zähler++) {
+    
+    if (zähler == 7 || zähler == 45) {
+        console.log(zähler)
+    } else {
+        
+    }
+};
+console.log("finish");
+
+// Zähle von 9 bis 99
+for (let zähler = 9; zähler<100; zähler++) {
+    
+    if (zähler == 9 || zähler == 99) {
+        console.log(zähler)
+    } else {
+        
+    }
+};
+console.log("finish");
+
+// Zähle rückwärts von 10 bis 0
+for (let zähler = 10; zähler>-1; zähler--) {
+    
+    if (zähler == 10 || zähler == 0) {
+        console.log(zähler)
+    } else {
+        
+    }
+};
+console.log("finish");
+// Zähle rückwärts von 1234 bis 1207
+for (let zähler = 1234; zähler>1206; zähler--) {
+    
+    if (zähler == 1234 || zähler == 1207) {
+        console.log(zähler)
+    } else {
+        
+    }
+};
+console.log("finish");
+
+// Gib Deinen Namen genau 8 mal aus
+for (let zahl = 0; zahl<8; zahl++) {
+    
+    console.log(zahl+1 + " Lukas")
+    
+};
+console.log("finish");
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for (let zahl = 0; zahl<11; zahl++) {
+    console.log(zahl)
+    if (zahl == 10) {
+        console.log("Lukas")
+        for (let zahl2 = 10; zahl2>-1; zahl2--) 
+            console.log(zahl2)
+
+    }else {
+       
+    }   
+}
