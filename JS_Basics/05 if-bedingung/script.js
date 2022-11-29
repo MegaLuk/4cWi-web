f588d2bd35486834738e674812af8d7490dbffd4
Lukas Rauch <lukas.rauch@student.htldornbirn.at>
2022-11-29 12:19:20 +0100

    if-bedingung

---
+++ JS_Basics/05 if-bedingung/script.js
@@ -0,0 +1,43 @@
+let number = 60
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+console.log("Aufgabe 1");
+if (number<20) {
+    console.log("mini");
+    
+} else if (number<50 && number>20) {
+        console.log("medium");
+
+    } else if (number>50) {
+        console.log("large");
+
+    } else {
+        console.log("halt was anderes")
+    };
+         
+    
+
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+console.log("Aufgabe 2");
+
+let number1 = 7
+
+if (number1<=7) {
+    console.log("mini")
+
+    
+} else if (number1>7 && number1<90) {
+    console.log("medium")
+} else if (number1>90) {
+    console.log("Large")
+} else {
+    console.log("halt was anderes")
+};
+    
+
