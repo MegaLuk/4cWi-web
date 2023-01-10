ef695dc4d416c2d702ff84b1b8483dd6c2170220
Lukas Rauch <lukas.rauch@student.htldornbirn.at>
2023-01-10 11:42:45 +0100

    kalender

--- JS_Basics/05 if-bedingung/script.js
+++ JS_Basics/05 if-bedingung/script.js
@@ -1,10 +1,12 @@
-let number = 60
+
 
 // Aufgabe 1
 // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
 // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
 // Wenn die Zahl größer als 50 ist gib aus "Large"
 console.log("Aufgabe 1");
+let number = 60
+
 if (number<20) {
     console.log("mini");
     
@@ -25,7 +27,6 @@ if (number<20) {
 // Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
 // Wenn die Zahl größer als 90 ist gib aus "Large"
 console.log("Aufgabe 2");
-
 let number1 = 7
 
 if (number1<=7) {
